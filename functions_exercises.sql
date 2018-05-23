e8ce210d20ce5a411928f4f9ad062ddaaf2ce884
Jake J <jakejohnson818@gmail.com>
2018-05-23 15:23:16 -0500

    Finished functions exercise

---
+++ functions_exercises.sql
@@ -0,0 +1,15 @@
+USE employees;
+
+
+SELECT CONCAT(first_name, ' ', last_name)
+FROM employees
+WHERE last_name LIKE 'E%'
+AND last_name LIKE '%e'
+ORDER BY emp_no DESC;
+
+
+SELECT DATEDIFF(CURDATE(), hire_date), CONCAT (first_name, ' ', last_name), birth_date
+FROM employees
+WHERE birth_date LIKE '19%-12-25'
+AND (hire_date BETWEEN '1990-01-01' AND '1999-12-31')
+ORDER BY birth_date ASC, hire_date DESC;
