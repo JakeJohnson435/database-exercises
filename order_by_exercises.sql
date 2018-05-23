b22fda8e57d38f2239fb47cc4846731ae850caa8
Jake J <jakejohnson818@gmail.com>
2018-05-23 13:59:25 -0500

    Finished exercise

---
+++ order_by_exercises.sql
@@ -0,0 +1,39 @@
+USE employees;
+
+SELECT first_name, last_name
+FROM employees
+WHERE gender = 'M'
+AND (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya')
+ORDER BY first_name DESC, last_name DESC;
+
+SELECT first_name, last_name
+FROM employees
+WHERE gender = 'M'
+AND (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya')
+ORDER BY last_name DESC, first_name DESC;
+
+SELECT first_name, last_name
+FROM employees
+WHERE last_name LIKE 'E%'
+OR last_name LIKE '%e'
+ORDER BY emp_no DESC;
+
+SELECT first_name, last_name
+FROM employees
+WHERE last_name LIKE 'E%'
+AND last_name LIKE '%e';
+
+SELECT first_name, last_name, hire_date, birth_date
+FROM employees
+WHERE birth_date LIKE '19%-12-25'
+AND (hire_date BETWEEN '1990-01-01' AND '1999-12-31')
+ORDER BY birth_date ASC, hire_date DESC;
+
+SELECT first_name, last_name, birth_date
+FROM employees
+WHERE birth_date LIKE '19%-12-25';
+
+SELECT first_name, last_name
+FROM employees
+WHERE last_name LIKE '%q%'
+AND last_name NOT LIKE '%qu%';
